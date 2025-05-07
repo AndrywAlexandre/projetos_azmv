f4794d13b0169755fd0c1e8ebc9c42890553dfac
andrywalexandre <andrywnatal@hotmail.com>
2025-05-07 00:08:30 -0300

    [ajuste] - exclusão do arquivo teste - descrição no codigo

--- posisao_relativ.py
+++ posisao_relativ.py
@@ -4,8 +4,10 @@ file_name= 'IMG_2149.mov'
 
 cap= cv2.VideoCapture(file_name)
 
+#Comando para execultar o video com sua abertura e seu fechamento
 while (cap.isOpened()):
     ret, frame=cap.read()
+#comando ate o primero brack para parar o video usando o 'Q'
     if ret == True:
         cv2.imshow('Frame',frame)
 
