c15b55e0a458e9880bf5de62514e8193756c5902
andrywalexandre <andrywnatal@hotmail.com>
2025-05-07 00:04:03 -0300

    [ajuste] - ajustado o ambiente virtual - alterado o caminho do video - ajustado o arquivo posisão-relativ

---
+++ posisao_relativ.py
@@ -0,0 +1,18 @@
+import cv2
+
+file_name= 'IMG_2149.mov'
+
+cap= cv2.VideoCapture(file_name)
+
+while (cap.isOpened()):
+    ret, frame=cap.read()
+    if ret == True:
+        cv2.imshow('Frame',frame)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cap.release()
+
+cv2.destroyAllWindows()
